8ae6e6d046a8fe80ac87f8bbf4050ac346fd3196
Chrissy Hsu <chrissy@ChrissydeMacBook-Pro.local>
2022-07-29 11:18:33 +0800

    fixed create wallet workflow bug

--- components/create.js
+++ components/create.js
@@ -64,11 +64,17 @@ export const CreateScreen = ({ navigation }) => {
 export const CreateScreen2 = ({ navigation, route }) => {
   const phrase = route.params.phrase.split(" ")
   const [check, setCheck] = useState([])
+  const [warning, setWarning] = useState(false)
 
   return (
     <View style={[styles.container, { paddingHorizontal: 40 }]}>
-      <View style={{flex: 1, alignItems:'center'}}>
-        <Text style={[styles.titleText, { fontSize: 24 }]}>確認您已經備份的註記詞</Text>
+      <View style={{flex: 1, alignItems:'center',}}>
+        { 
+          warning ?
+          <Text style={[styles.titleText, { fontSize: 24, color: 'red' }]}>註記詞錯誤</Text> 
+          :
+          <Text style={[styles.titleText, { fontSize: 24 }]}>確認您已經備份的註記詞</Text>
+        }
       </View>
       <View style={[styles.boxContainer, { flexWrap: 'wrap' }]}>
         {
@@ -83,6 +89,7 @@ export const CreateScreen2 = ({ navigation, route }) => {
                   let one = check.slice(0, check.indexOf(e))
                   let two = check.slice(check.indexOf(e)+1, check.length)
                   setCheck(() => [...one, ...two])
+                  setWarning(false)
                 }}
               />
             </View>
@@ -118,7 +125,8 @@ export const CreateScreen2 = ({ navigation, route }) => {
           ?
           <Button 
             onPress={() => {
-              if(check.every((e, index) => e === phrase[index])) {
+              if(check.every((e, index) => e != phrase[index])) setWarning(true) 
+              else {
                 navigation.goBack() 
                 navigation.goBack()
                 navigation.navigate("Home", {addr: route.params.address, mnem: route.params.phrase})
